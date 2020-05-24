893e2c0957679c8ab706eb78bbf79f04418409da
kotsur-vlad <kotsur.vlad@gmail.com>
2020-05-24 13:02:45 +0300

    Updates

---
+++ precache-manifest.ac2c9ea9d9ab13fe6d399c06868910cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50f4a6ca1895f1ef833a9235d24d183",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "e2f3883006ca7daaa3a7",
+    "url": "/repository-name/static/css/main.0ddd5838.chunk.css"
+  },
+  {
+    "revision": "9186c6629cda495f8f0e",
+    "url": "/repository-name/static/js/2.c8a87a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.c8a87a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f3883006ca7daaa3a7",
+    "url": "/repository-name/static/js/main.dc646be3.chunk.js"
+  },
+  {
+    "revision": "5db6764b9f54ce1233f4",
+    "url": "/repository-name/static/js/runtime-main.b206b48f.js"
+  },
+  {
+    "revision": "6b78238d5005eee83df6e971c9661b37",
+    "url": "/repository-name/static/media/background.6b78238d.jpg"
+  },
+  {
+    "revision": "f7c82cf8f26b5e0911bddb5877feead1",
+    "url": "/repository-name/static/media/smashSound.f7c82cf8.mp3"
+  }
+]);
